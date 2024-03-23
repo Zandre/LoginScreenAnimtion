0ba766cb2bafc114a2931720377af27bbb57ed51
Zandre Botha <botha.zandre@gmail.com>
2024-03-23 09:10:41 +0100

    dotted map works

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -22,8 +22,8 @@ namespace WebApplication1
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/flow").Include(
-                        "~/Scripts/flow.js"));
+            bundles.Add(new ScriptBundle("~/bundles/dottedmap").Include(
+                        "~/Scripts/dottedmap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
